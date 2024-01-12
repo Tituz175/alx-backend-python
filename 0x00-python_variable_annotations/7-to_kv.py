3cd44e30e636d8d1f180f0dc264083723b0e961f
tituz175 <tobititus01@gmail.com>
2024-01-12 16:35:23 -0700

    task 7 modified

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,10 +2,10 @@
 """7. Complex types - string and int/float to tuple"""
 
 
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str,  v: Union[float, int]) -> tuple[str, Union[int, float]]:
+def to_kv(k: str,  v: Union[float, int]) -> Tuple[str, Union[int, float]]:
     """
     This function takes a list mxd_lst of floats as argument
     and returns their sum as a float.
