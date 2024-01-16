96559a107d2ad357a605d2fde6b007971824e131
tituz175 <tobititus01@gmail.com>
2024-01-15 19:57:27 -0700

    task 3 done

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """Module Documentation"""
 import asyncio
-from typing import Any
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Any:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """Function Documentation"""
     return asyncio.create_task(wait_random(max_delay))
