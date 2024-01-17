70154ae676886d6207cd41fff94d2ba072f6ae27
tituz175 <tobititus01@gmail.com>
2024-01-16 19:50:32 -0700

    task 0 done

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,5 +8,5 @@ from typing import Generator
 async def async_generator() -> Generator[float, None, None]:
     """Fuction Documentation"""
     for _ in range(10):
-        await asyncio.sleep(1.0)
+        await asyncio.sleep(1)
         yield random.uniform(0, 10)
