767419d5341077922d0e056918b031a112717311
tituz175 <tobititus01@gmail.com>
2024-01-16 19:39:19 -0700

    task 0 done

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Module Documentation"""
 import random
+import asyncio
 
 
 async def async_generator() -> float:
     """Fuction Documentation"""
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1.0)
         yield (random.uniform(0, 10))
